567aa9e2f666873d80770c4a1f0474647b2e27fd
Jake Champion <me@jakechampion.name>
2024-11-19 09:30:56 +0000

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -2,8 +2,10 @@ import { NextResponse } from 'next/server'
  
 // This function can be marked `async` if using `await` inside
 export function middleware(request) {
+  console.log('in the next middleware');
   let response = NextResponse.next()
   response.cookies.set('show-banner', 'false')
+  console.log('leaving the next middleware')
   return response
 }
  
