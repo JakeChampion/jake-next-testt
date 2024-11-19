8786f24b4babb36eee17d64baf7f427a3e2294dd
jake champion <jake.champion@netlify.com>
2024-11-19 09:40:59 +0000

    ok

--- netlify/edge-functions/rewrite.js
+++ netlify/edge-functions/rewrite.js
@@ -1,5 +1,7 @@
 const rewrite = async (request, context) => {
+    console.log('in the rewrite middleware', request.headers.get('x-nf-request-id'));
     const path = context.geo?.country?.code === 'AU' ? '/edge/australia' : '/edge/not-australia';
+    console.log('leaving the rewrite middleware', request.headers.get('x-nf-request-id'))
     return new URL(path, request.url);
 };
 
